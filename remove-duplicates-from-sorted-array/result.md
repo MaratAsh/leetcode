69740e6c552acd66e5b73cfa157665d51e122d09
Marat Ashrafzianov <marat.ashraf@yandex.ru>
2023-09-09 21:36:57 +0300

    remove-duplicates-from-sorted-array problem solved

---
+++ remove-duplicates-from-sorted-array/result.md
@@ -0,0 +1,5 @@
+# Results
+
+Time Submitted|Status|Runtime|Memory|Language|Time Complexity
+--- | --- | --- | --- | ---
+09/09/2023 21:26|Accepted|79 ms|18.14 MB|python3|O(n)
---
+++ remove-duplicates-from-sorted-array/solution.py
@@ -0,0 +1,26 @@
+from typing import List
+from collections import deque
+
+inf = float("inf")
+
+class Solution:
+    def removeElement(self, nums: List[int]) -> int:
+        deq = deque()
+        last = float("-inf")
+        keep_count = 0
+        for i in range(len(nums)):
+            if last < nums[i]:
+                keep_count += 1
+                last = nums[i]
+                if len(deq) > 0:
+                    nums[deq.popleft()] = last
+                    nums[i] = inf
+                    deq.append(i)
+            else:
+                nums[i] = inf
+                deq.append(i)
+                    
+            
+        return keep_count
+
+
